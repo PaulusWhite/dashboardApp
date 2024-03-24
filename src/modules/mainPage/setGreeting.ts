1ce6849bc77c6af7b8a3a553fb0919294f08ee10
Pavel <exactlypaulus@gmail.com>
2024-03-24 20:36:37 +0300

    feat: implement clock timer on the main page

--- src/modules/mainPage/setGreeting.ts
+++ src/modules/mainPage/setGreeting.ts
@@ -4,18 +4,28 @@ import getCurrentTime from "../../utils/getCurrentTime";
 //storage Modules
 import { getUserName } from "../../storage/getSetUserName";
 
+const SEC_PER_MIN: number = 60;
+
 const setGreeting = (): void => {
   const greetingTime: HTMLParagraphElement = document.querySelector(".greeting__time") as HTMLParagraphElement;
   const greetingName: HTMLParagraphElement = document.querySelector(".greeting__name") as HTMLParagraphElement;
 
-  const [hours, minutes] = getCurrentTime();
-  greetingTime.innerHTML = `${hours}:${minutes}`;
+  const [hours, minutes, seconds] = getCurrentTime();
+  greetingTime.textContent = `${hours}:${minutes}`;
 
   let userName: string | null = getUserName();
   userName = userName ? userName : "Buddy";
 
   const greetingPhrase: string = `Hello, dear ${userName}`;
-  greetingName.innerHTML = greetingPhrase;
+  greetingName.textContent = greetingPhrase;
+
+  const secTillNextMin: number = SEC_PER_MIN - +seconds;
+
+  console.log(secTillNextMin)
+
+  setTimeout( () => {
+    setGreeting();
+  }, +`${secTillNextMin}000`);
 }
 
 export default setGreeting;
