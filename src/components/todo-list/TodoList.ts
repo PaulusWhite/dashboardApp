0d94cc87d370ff05c341354db8e488b0dfff473e
Pavel <exactlypaulus@gmail.com>
2024-04-10 19:43:06 +0300

    feat: implement basic view of todo-list app

---
+++ src/components/todo-list/TodoList.ts
@@ -0,0 +1,30 @@
+const TodoList = (): string => {
+  const view = `
+    <div class="todo-list">
+      <h1>Todo Lists</h1>
+
+      <div class="tasks">
+
+        <div class="tasks__all-tasks tasks__tasks-group">
+          <h2>Tasks</h2>
+          <div class="tasks__tasks-list"></div>
+        </div>
+
+        <div class="tasks__important-tasks tasks__tasks-group">
+          <h2>Important</h2>
+          <div class="tasks__tasks-list"></div>
+        </div>
+
+      </div>
+
+      <div class="todo-list__input-field">
+        <input id="todo-list__input" type="text" placeholder="What do you need to do?">
+        <button class="todo-list__add-btn">Add</button>
+      </div>
+    </div>
+  `
+
+  return view;
+}
+
+export default TodoList;
--- src/modules/setMainPage.ts
+++ src/modules/setMainPage.ts
@@ -2,11 +2,14 @@
 import MainPage from "../components/MainPage";
 import UserNameInputWindow from "../components/UserNameInputWindow";
 
+import TodoList from "../components/todo-list/TodoList";
+
 const setMainPage = (): void => {
   const body: HTMLBodyElement = document.body as HTMLBodyElement;
 
   body.innerHTML += MainPage();
   body.innerHTML += UserNameInputWindow();
+  body.innerHTML += TodoList();
 }
 
 export default setMainPage;
