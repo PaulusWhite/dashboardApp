efafe980aafd88cc28bc8dea597d25909bbf1822
Pavel <exactlypaulus@gmail.com>
2024-04-14 13:22:53 +0300

    refactor: make some refactoring

--- src/assets/icons/EditIcon.ts
+++ src/assets/icons/EditIcon.ts
@@ -5,9 +5,9 @@ const EditIcon = (): string => {
     <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
     <g id="SVGRepo_iconCarrier"> <path d="M15 6.5L17.5 9M11 20H20M4 20V17.5L16.75 4.75C17.4404 4.05964 18.5596 4.05964 19.25 4.75V4.75C19.9404 5.44036 19.9404 6.55964 19.25 7.25L6.5 20H4Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> </g>
   /svg>
-  `
+  `;
 
   return view;
-}
+};
 
 export default EditIcon;
