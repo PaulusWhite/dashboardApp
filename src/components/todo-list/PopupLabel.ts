d735526a95d84c76c01ce571b4f187c47e5d46b1
Pavel <exactlypaulus@gmail.com>
2024-04-14 12:18:43 +0300

    feat: add popup labels under the btns

---
+++ src/components/todo-list/PopupLabel.ts
@@ -0,0 +1,8 @@
+const PopupLabel = (text: string): string => {
+  const view = `
+    <span class="popup-label">${text}</span>
+  `
+  return view;
+}
+
+export default PopupLabel;
--- src/components/todo-list/Task.ts
+++ src/components/todo-list/Task.ts
@@ -1,3 +1,6 @@
+//Components
+import PopupLabel from "./PopupLabel";
+
 //icons
 import checkIcon from "./../../assets/icons/check.svg";
 import starIcon from "./../../assets/icons/star.svg";
@@ -7,17 +10,20 @@ const Task = (): string => {
     <div class="task">
       <button class="task__complete-btn">
         <img src="${checkIcon}" alt="check-icon">
+        ${PopupLabel("Mark as completed")}
       </button>
       <p class="task__text">Get a job in IT sphere</p>
 
       <div class="task__tools">
         <button class="task__important-btn">
           <img src="${starIcon}" alt="star-icon">
+          ${PopupLabel("Mark as important")}
         </button>
         <button class="task__options">
           <span class="task__options-circle"></span>
           <span class="task__options-circle"></span>
           <span class="task__options-circle"></span>
+          ${PopupLabel("More options")}
         </button>
       </div>
       
