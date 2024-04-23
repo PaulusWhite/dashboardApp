6814b512c955f5b575945915ad10c06fa66faacb
Pavel <exactlypaulus@gmail.com>
2024-04-23 16:19:44 +0300

    refactor: refactor editing of task bullet point

--- src/modules/todoApp/updateBulletPoint.ts
+++ src/modules/todoApp/updateBulletPoint.ts
@@ -1,29 +1,40 @@
 //Interfaces
-import { TPageClass } from "../../interfaces/ITodoList";
+import { TPageClass, TBulletPointType, IUpdBulletPointData } from "../../interfaces/ITodoList";
 
 //Modules
 import updateTodoListsData from "./updateTodoListsData";
 
-// const displayEditMode = (taskElement: HTMLLIElement, editModeField: HTMLDivElement, textField: HTMLParagraphElement) => {
-//   taskElement.classList.toggle("bullet-point__edit-mode");
-//   editModeField.classList.toggle("none");
-//   textField.classList.toggle("none");
-// };
+const displayEditMode = (bulletPoint: HTMLLIElement, editModeField: HTMLDivElement, textField: HTMLParagraphElement) => {
+  bulletPoint.classList.toggle("bullet-point__edit-mode");
+  editModeField.classList.toggle("none");
+  textField.classList.toggle("none");
+};
 
-// const getEditModeElements = (taskElement: HTMLLIElement) => {
-//   const editModeField: HTMLDivElement = taskElement.children[2] as HTMLDivElement;
-//   const editInput: HTMLInputElement = editModeField.firstElementChild as HTMLInputElement;
-//   const textField: HTMLParagraphElement = taskElement.children[1] as HTMLParagraphElement;
+const getEditModeElements = (bulletPoint: HTMLLIElement) => {
+  const editModeField: HTMLDivElement = bulletPoint.children[2] as HTMLDivElement;
+  const editInput: HTMLInputElement = editModeField.firstElementChild as HTMLInputElement;
+  const textField: HTMLParagraphElement = bulletPoint.children[1] as HTMLParagraphElement;
 
-//   return { editModeField, editInput, textField };
-// };
+  return { editModeField, editInput, textField };
+};
+
+const editBulletPointTextAction = (bulletPoint: HTMLLIElement, bulletPointId: string, bulletPointType: TBulletPointType) => {
+  const { editInput, textField, editModeField } = getEditModeElements(bulletPoint);
+
+  displayEditMode(bulletPoint, editModeField, textField);
+
+  const newBulletPointTextValue: string = editInput.value.replace(/\s{2,}/g, " ").trim();
+
+  const updData: IUpdBulletPointData = {
+    text: newBulletPointTextValue,
+  };
+
+  const isBulletPointRemoved: boolean = !newBulletPointTextValue ? true : false; //if input is empty then bullet point should be deleted
+
+  updateTodoListsData({ bulletPointId, updData, updDataType: bulletPointType, isRemove: isBulletPointRemoved });
+};
 
 const markTaskAction = (taskElement: HTMLLIElement, taskID: string, type: "importance" | "status") => {
-  // interface IUpdTaskData {
-  //   isImportant?: boolean,
-  //   isCompleted?: boolean,
-  // }
-
   const markClass = type === "importance" ? "bullet-point__important" : "bullet-point__completed";
   taskElement.classList.toggle(markClass);
 
@@ -40,62 +51,53 @@ const markTaskAction = (taskElement: HTMLLIElement, taskID: string, type: "impor
   });
 };
 
-// const setEditModeAction = (taskElement: HTMLLIElement, taskID: string) => {
-//   const { editInput, textField, editModeField } = getEditModeElements(taskElement);
+const setEditModeAction = (
+  bulletPoint: HTMLLIElement,
+  bulletPointId: string,
+  pageClass: TPageClass,
+  bulletPointType: TBulletPointType,
+) => {
+  const { editInput, textField, editModeField } = getEditModeElements(bulletPoint);
 
-//   displayEditMode(taskElement, editModeField, textField);
+  displayEditMode(bulletPoint, editModeField, textField);
 
-//   const todoList: HTMLDivElement = document.querySelector(".todo-list") as HTMLDivElement;
+  const page: HTMLDivElement = document.querySelector(`.${pageClass}`) as HTMLDivElement;
 
-//   editInput.value = textField.innerHTML;
-//   editInput.focus();
+  editInput.value = textField.innerHTML;
+  editInput.focus();
 
-//   const editTaskWithKeuboardAction = (event: KeyboardEvent) => {
-//     if (event.code === "Enter" || event.code === "NumpadEnter") {
-//       editTaskTextAction(taskElement, taskID);
-//     }
-//   };
+  const editTaskWithKeuboardAction = (event: KeyboardEvent) => {
+    if (event.code === "Enter" || event.code === "NumpadEnter") {
+      editBulletPointTextAction(bulletPoint, bulletPointId, bulletPointType);
+    }
+  };
 
-//   const removeEditMode = (event: Event) => {
-//     const target: HTMLElement = event.target as HTMLElement;
-//     const clickedTaskInput: HTMLInputElement | null = target.closest(".task__edit-input");
-//     const clickedTask: HTMLDivElement | null = target.closest(".task");
+  const removeEditMode = (event: Event) => {
+    const target: HTMLElement = event.target as HTMLElement;
+    const clickedBulletPointInput: HTMLInputElement | null = target.closest(".bullet-point__edit-input");
+    const clickedBulletPoind: HTMLDivElement | null = target.closest(".bullet-point");
 
-//     if (clickedTask && clickedTask.id === taskID && clickedTaskInput) return;
+    if (clickedBulletPoind && clickedBulletPoind.id === bulletPointId && clickedBulletPointInput) return;
 
-//     displayEditMode(taskElement, editModeField, textField);
-//     todoList.removeEventListener("click", removeEditMode);
-//     taskElement.removeEventListener("keydown", editTaskWithKeuboardAction);
-//   };
+    displayEditMode(bulletPoint, editModeField, textField);
 
-//   taskElement.addEventListener("keydown", editTaskWithKeuboardAction);
+    page.removeEventListener("click", removeEditMode);
+    bulletPoint.removeEventListener("keydown", editTaskWithKeuboardAction);
+  };
 
-//   setTimeout(() => {
-//     todoList.addEventListener("click", removeEditMode);
-//   }, 0);
-// };
+  bulletPoint.addEventListener("keydown", editTaskWithKeuboardAction);
 
-// const editTaskTextAction = (taskElement: HTMLLIElement, taskID: string) => {
-//   const { editInput, textField, editModeField } = getEditModeElements(taskElement);
-
-//   displayEditMode(taskElement, editModeField, textField);
-
-//   const newTaskText: string = editInput.value.replace(/\s{2,}/g, " ").trim();
-
-//   const updatedTaskData: IUpdatedTaskData = {
-//     text: newTaskText,
-//   };
-
-//   const isTaskRemoved: boolean = !newTaskText ? true : false;
-
-//   updateTodoListsData(taskID, updatedTaskData, isTaskRemoved);
-// };
+  setTimeout(() => {
+    page.addEventListener("click", removeEditMode);
+  }, 0);
+};
 
 const updateBulletPoint = (pageClass: TPageClass) => {
   const page: HTMLDivElement = document.querySelector(`.${pageClass}`) as HTMLDivElement;
 
   page.addEventListener("click", (event: Event) => {
     const target: HTMLElement = event.target as HTMLElement;
+    const bulletPointType: TBulletPointType = pageClass === "my-todo-lists" ? "list" : "task";
 
     if (!target.closest(".bullet-point")) return;
 
@@ -111,11 +113,11 @@ const updateBulletPoint = (pageClass: TPageClass) => {
     }
 
     // //updating with popup-options menu (delete and edit)
-    if (target.closest(".popup-options__delete-btn"))
+    if (target.closest(".popup-options__delete-btn")) {
       updateTodoListsData({ updDataType: "task", bulletPointId, updData: {}, isRemove: true });
-
-    // if (target.closest(".popup-options__edit-btn")) setEditModeAction(taskElement, taskID);
-    // if (target.closest(".bullet-point__edit-btn")) editTaskTextAction(taskElement, taskID);
+    }
+    if (target.closest(".popup-options__edit-btn")) setEditModeAction(bulletPoint, bulletPointId, pageClass, bulletPointType);
+    if (target.closest(".bullet-point__edit-btn")) editBulletPointTextAction(bulletPoint, bulletPointId, bulletPointType);
   });
 };
 
